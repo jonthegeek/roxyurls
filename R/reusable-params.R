5720498f303e95a6f0629cb19f2da99c475c07f3
Jon Harmon <jon.harmon@atorusresearch.com>
2024-07-17 14:31:14 -0500

    Initial commit

---
+++ R/reusable-params.R
@@ -0,0 +1,40 @@
+#' Function a
+#'
+#' A function to document params.
+#'
+#' More details. And then I'll load details.
+#' ```{r child = "man/glossary/b_result4.Rmd"}
+#' ```
+#'
+#' ```{r child = "man/glossary/roxygenload.Rmd"}
+#' ```
+#'
+#' @param b_result1 The result from [fnb()].
+#' @param b_result2
+#' The result from [fnb()].
+#' @param b_result3
+#' The result from [fnb()].
+#'
+#' [fnb()]: https://google.com
+#' @param b_result4
+#' ```{r child = "man/glossary/b_result4.Rmd"}
+#' ```
+#' @param roxygen_reference
+#' ```{r child = "man/glossary/roxygenload.Rmd"}
+#' ```
+#'
+#' @return Things
+#' @export
+fna <- function(b_result1, b_result2, b_result3) {
+  #
+}
+
+#' Function b
+#'
+#' A function to reference.
+#'
+#' @return Things
+#' @export
+fnb <- function() {
+  #
+}
